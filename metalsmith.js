5648ffab16cb300913fc9e9ee933090361d33661
Toilal <toilal.dev@gmail.com>
2017-07-02 15:24:50 +0200

    Remove vue-cli dependencies usage

--- metalsmith.js
+++ metalsmith.js
@@ -1,22 +1,25 @@
 var copy = require('./lib/metalsmith-copy')
-var minimatch = require('minimatch')
+
+var srcRegexp = /^src[\/|\\]/g
+var libRegexp = /^(.*\.)(lib\.)(ts|js)$/g
+var componentsRegexp = /^(.*)([\/|\\]components)([\/|\\].*)$/g
 
 module.exports = function (metalsmith, opts, helpers) {
   function libraryProjectCopy (files, metalsmith, done) {
     if (metalsmith.metadata().projectType === 'lib') {
-      var libRegexp = /^(.*\.)(lib\.)(ts|js)$/g;
-
       copy({
         move: true,
-        pattern: "src/**/*",
         transform: function (file) {
           // Move all files but .lib files and components directory to app directory
-          if (!minimatch(file, "**/*.lib.+(ts|js)") && !minimatch(file, "**/components/**/*")) {
-            file = 'app' + file.substring(3)
-          }
+          if (file.match(srcRegexp)) {
+            console.log(file)
+            if (!file.match(libRegexp) && !file.match(componentsRegexp)) {
+              file = 'app' + file.substring(3)
+            }
 
-          // Rename .lib files
-          file = file.replace(libRegexp, '$1$3')
+            // Rename .lib files
+            file = file.replace(libRegexp, '$1$3')
+          }
           return file;
         }
       })(files, metalsmith, done)
