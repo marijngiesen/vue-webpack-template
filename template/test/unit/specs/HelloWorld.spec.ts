f8517fee5d4300bcbd78b9c196a236bb2858c8af
Toilal <toilal.dev@gmail.com>
2017-12-13 20:34:16 +0100

    Add strict option to TypeScript configuration

--- template/test/unit/specs/HelloWorld.spec.ts
+++ template/test/unit/specs/HelloWorld.spec.ts
@@ -6,7 +6,7 @@ describe('HelloWorld.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(HelloWorld)
     const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
+    expect(vm.$el.querySelector('.hello h1')!.textContent)
     {{#if_eq runner "karma"}}.to.equal('Welcome to Your Vue.js App'){{/if_eq}}{{#if_eq runner "jest"}}.toEqual('Welcome to Your Vue.js App'){{/if_eq}}
   })
 })
